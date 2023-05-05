24610dc24016968b0e0dba6c403482a09a4b04aa
zinoo-jsx <zinoo.dev@gmail.com>
2023-05-05 15:31:58 +0630

    add in web

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+echo "hi";
 require __DIR__ . '/auth.php';
 
 // query builder
